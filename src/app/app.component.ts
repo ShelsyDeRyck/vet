b41e94723f5fc5d549350b0c3d2fb01fe8a16452
ShelsyDeRyck <114619980+ShelsyDeRyck@users.noreply.github.com>
2024-05-16 19:48:04 +0200

    project start + already made front page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl , Validators} from '@angular/forms';
+// import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pawsibilities';
+  searchForm = new FormGroup({
+    name: new FormControl(""),
+    city: new FormControl(""),
+    type: new FormControl("")
+  });
+
+  onSubmit(event) {
+    event.preventDefault()
+    const formData = this.searchForm.value;
+    formData.type = this.selectedValue;
+    console.log(formData);
+  }
+
+  selectedValue: string;
+  options = [
+    { value: 'household-pets', label: 'household pets' },
+    { value: 'reptile', label: 'Reptiles' },
+    { value: 'ruminants', label: 'Ruminants (Cows & Horses)' },
+    { value: 'aquatic', label: 'Aquatic' },
+    { value: 'rodents', label: 'Rodents' },
+    { value: 'exotic', label: 'Exotic' },
+    { value: 'zoo', label: 'Zoo' }
+  ];
+  
+
+  onRadioChange(value: string) {
+    this.selectedValue = value;
+  }
 }
